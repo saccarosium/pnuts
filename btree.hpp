7146bd544b0494e01efa6dbb532dcf405e9b1457
Luca Saccarola <github.e41mv@aleeas.com>
2023-12-30 13:21:33 +0100

    Remove btreeBuildUnbalanced

--- btree.hpp
+++ btree.hpp
@@ -6,14 +6,8 @@ struct Node {
     Node* right;
 };
 
-enum Direction {
-    LEFT,
-    RIGHT,
-};
-
 typedef Node* PNode;
 
 PNode btreeBuildBalanced(int h, bool random);
-PNode btreeBuildUnBalanced(int h, Direction d);
 void btreePrint(const PNode t);
 void btreeFlush(PNode t);
