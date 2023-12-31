8826dff92b232f714390d16eac8bf3b101465834
Luca Saccarola <github.e41mv@aleeas.com>
2023-12-31 16:16:47 +0100

    Adding BFS and DFS for btrees

--- btree.cpp
+++ btree.cpp
@@ -1,6 +1,6 @@
 #include "btree.hpp"
 #include <iostream>
-#include <random>
+#include <queue>
 
 static PNode btBuildBalancedAux(int h, int n) {
     if (h-- < 0)
@@ -10,7 +10,44 @@ static PNode btBuildBalancedAux(int h, int n) {
 }
 
 PNode btBuildBalanced(int h) {
-    return btBuildBalancedAux(h, 0);
+    return btBuildBalancedAux(h, 1);
+}
+
+static void btDFSAux(const PNode t, std::vector<std::optional<int>>& v) {
+    if (!t) {
+        v.push_back({});
+        return; 
+    }
+
+    btDFSAux(t->left, v);
+    v.push_back(t->key);
+    btDFSAux(t->right, v);
+}
+
+std::vector<std::optional<int>> btDFS(const PNode t) {
+    std::vector<std::optional<int>> v;
+    btDFSAux(t, v);
+    return v;
+}
+
+std::vector<std::optional<int>> btBFS(const PNode t) {
+    std::queue<PNode> q;
+    std::vector<std::optional<int>> v;
+
+    q.push(t);
+    while (!q.empty()) {
+        PNode tmp = q.front();
+        if (tmp) {
+            v.push_back(tmp->key);
+            q.push(tmp->left);
+            q.push(tmp->right);
+        } else {
+            v.push_back({});
+        }
+        q.pop();
+    }
+
+    return v;
 }
 
 static void btPrintAux(const std::string& prefix, const PNode node,
@@ -49,5 +86,12 @@ void btFlush(PNode t) {
 int main() {
     PNode root = btBuildBalanced(2);
     btPrint(root);
+    std::vector<std::optional<int>> v = btBFS(root);
+    for (const auto& x : v)
+        if (x)
+            std::cout << x.value() << std::endl;
+        else
+            std::cout << 'N' << std::endl;
+    /* btPrint(root); */
     btFlush(root);
 }
--- btree.hpp
+++ btree.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <optional>
+#include <vector>
+
 struct Node {
     int key;
     Node* left;
@@ -8,7 +11,11 @@ struct Node {
 
 typedef Node* PNode;
 
+PNode btBuild(const std::vector<int>&& v);
 PNode btBuildBalanced(int h);
+
+std::vector<std::optional<int>> btDFS(const PNode t);
+std::vector<std::optional<int>> btBFS(const PNode t);
 
 void btPrint(const PNode t);
 void btFlush(PNode t);
